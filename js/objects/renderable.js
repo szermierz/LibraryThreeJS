ef04c13d7e665d36b58650432c68d91ef8791e4b
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2015-12-16 22:05:25 +0100

    Some changes

---
+++ js/objects/renderable.js
@@ -0,0 +1,25 @@
+Renderable = function(Material, Geometry)
+{
+	// Fields
+	this.m_material = Material;
+	this.m_geometry = Geometry;
+	this.m_renderableObject = new THREE.Mesh(this.m_geometry, this.m_material);
+	this.m_isInitialized = true;
+	
+	// Methods
+	this.GetSceneObject = function()
+	{
+		if(!this.m_isInitialized)
+			return null;
+		
+		return this.m_renderableObject;
+	}
+	
+	this.CreateCopySceneObject = function()
+	{
+		if(!this.m_isInitialized)
+			return null;
+		
+		return new THREE.Mesh(this.m_geometry, this.m_material);
+	}
+}
