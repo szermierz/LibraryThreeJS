54adcaee1c450c89e757374b07b507405149cfab
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2015-12-17 21:06:57 +0100

    Restored working loadng

--- js/core/events.js
+++ js/core/events.js
@@ -1,14 +1,29 @@
 
 var OnLoaded = function()
 {
-	//todo
+	console.log('Finished loading resources!');
+
+	RefreshFloor();
+	LoadLibraryCamera();
 	
-	console.log("Loaded resources!");
+	var geo = new THREE.PlaneGeometry(0.5, 0.5);
+	var mat = new THREE.MeshBasicMaterial( {color: 0xDDDDDD} );
+	
+	var mesh = new THREE.Mesh(geo, mat);
+	mesh.position.x = 50.0;
+	mesh.position.y = 50.0;
+	mesh.position.z = 0.0;
+	mesh.rotation.x = 90.0;
+	mesh.rotation.y = 0.0;
+	
+	g_MainScene.add(mesh);
 }
 
 var OnUpdate = function()
 {
-	//todo
+	g_HumanDirection += 1.0;
 	
-	console.log("OnUpdate");//todo wywalic to
+	RefreshCamera();
+
+	//g_MainCamera.lookAt(50,50,0);
 }
