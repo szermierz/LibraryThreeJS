1eb9866f654e780ccf66464f6593a46dd81d3755
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2015-12-17 21:01:00 +0100

    Revert "some shit"
    This reverts commit 9ba54bd30669b43aeb369b96da794303939abf76.

--- js/core/events.js
+++ js/core/events.js
@@ -1,29 +1,14 @@
 
 var OnLoaded = function()
 {
-	console.log('Finished loading resources!');
+	//todo
 	
-	RefreshFloor();
-	LoadLibraryCamera();
-	
-	var geo = new THREE.PlaneGeometry(0.5, 0.5);
-	var mat = new THREE.MeshBasicMaterial( {color: 0xDDDDDD} );
-	
-	var mesh = new THREE.Mesh(geo, mat);
-	mesh.position.x = 50.0;
-	mesh.position.y = 50.0;
-	mesh.position.z = 0.0;
-	mesh.rotation.x = 90.0;
-	mesh.rotation.y = 0.0;
-	
-	g_MainScene.add(mesh);
+	console.log("Loaded resources!");
 }
 
 var OnUpdate = function()
 {
-	g_HumanDirection += 1.0;
+	//todo
 	
-	RefreshCamera();
-	
-	//g_MainCamera.lookAt(50,50,0);
+	console.log("OnUpdate");//todo wywalic to
 }
